f293e533870257553e31ab00c35ac7167cb110ce
studiobigli <liamcross1986@protonmail.com>
2025-05-02 18:18:21 +0100

    Module returns data as bytestream allowing libraries to handle file output

--- test/conftest.py
+++ test/conftest.py
@@ -7,3 +7,11 @@ def dummy_file(tmp_path, filename="dummydata.csv"):
     d.mkdir()
     p = d / filename
     return [p, tmp_path, d, filename]
+
+
+@pytest.fixture(scope="function")
+def dummy_output_file(tmp_path, filename="dummydata-obfuscated.csv"):
+    d = tmp_path / "obfuscated"
+    d.mkdir()
+    p = d / filename
+    return [p, tmp_path, d, filename]
