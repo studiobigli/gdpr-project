a7f98a3064d2ac3d7d06bbe2623615a243501d2b
studiobigli <liamcross1986@protonmail.com>
2025-05-04 15:55:34 +0100

    Pass with Black and Bandit

--- src/example_lambda.py
+++ src/example_lambda.py
@@ -11,23 +11,26 @@ def lambda_handler(event, context):
         s3 = boto3.client("s3")
         byte_stream = io.BytesIO()
 
-        s3.download_fileobj(Bucket=os.environ["INGESTION_BUCKET"], Key=os.environ["INGESTION_KEY"], Fileobj=byte_stream)
+        s3.download_fileobj(
+            Bucket=os.environ["INGESTION_BUCKET"],
+            Key=os.environ["INGESTION_KEY"],
+            Fileobj=byte_stream,
+        )
         byte_stream.seek(0)
 
         result = obfuscate(columns, byte_stream)
-    
+
         o_key = i_filepath[1].rsplit(".", 1)
         o_key[0] += "-obfuscated."
         o_key = "".join(o_key)
 
         s3.put_object(Bucket=os.environ["OBFUSCATED_BUCKET"], Body=result, Key=o_key)
 
-        return { 
+        return {
             "response": 200,
             "bucket": os.environ["OBFUSCATED_BUCKET"],
-            "key": o_key
+            "key": o_key,
         }
 
     except Exception as e:
         return {"response": 500, "error": e}
-
--- test/test_example_lambda.py
+++ test/test_example_lambda.py
@@ -13,10 +13,10 @@ def dummy_s3():
     test_good_data = "id,First Name,Last Name,Age\n1,aaa,aaa,20\n2,bbb,bbb,302"
     test_good_columns = ["id", "First Name", "Last Name", "Age"]
     test_key = "dummy.csv"
-    
-    os.environ['INGESTION_BUCKET'] = ingestion_bucket
-    os.environ['OBFUSCATED_BUCKET'] = obfuscated_bucket
-    os.environ['INGESTION_KEY'] = test_key
+
+    os.environ["INGESTION_BUCKET"] = ingestion_bucket
+    os.environ["OBFUSCATED_BUCKET"] = obfuscated_bucket
+    os.environ["INGESTION_KEY"] = test_key
 
     with mock_aws():
         s3 = boto3.client("s3")
@@ -39,10 +39,10 @@ class TestFunctionLambdaHandler:
             "key": "dummy.csv",
             "fields": ["id", "First Name", "Last Name", "Age"],
         }
-        
+
         response = lambda_handler(test_event, {})
-        check = dummy_s3.list_objects(Bucket="test-obfuscated") 
-        
+        check = dummy_s3.list_objects(Bucket="test-obfuscated")
+
         assert response["response"] == 200
         assert check["Contents"][0]["Key"] == response["key"]
 
@@ -56,6 +56,7 @@ class TestFunctionLambdaHandler:
 
         assert response["response"] == 500
 
+
 class TestOutputData:
     def test_obfuscated_file_contains_correct_data(self, dummy_s3):
         check_data = "id,First Name,Last Name,Age\n1,***,aaa,***\n2,***,bbb,***\n"
@@ -67,6 +68,4 @@ class TestOutputData:
 
         response = lambda_handler(test_event, {})
         obj = dummy_s3.get_object(Bucket="test-obfuscated", Key="dummy-obfuscated.csv")
-        assert obj['Body'].read().decode('utf-8') == check_data 
-
-
+        assert obj["Body"].read().decode("utf-8") == check_data
