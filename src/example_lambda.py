3d83bc7ce2fc29ea069836774b0f9cf077a9f4c5
studiobigli <liamcross1986@protonmail.com>
2025-05-04 15:41:40 +0100

    Initial Lambda functions within mock AWS environment

---
+++ src/example_lambda.py
@@ -0,0 +1,33 @@
+from src.obfuscator import obfuscate
+import boto3
+import io
+import os
+
+
+def lambda_handler(event, context):
+    try:
+        i_filepath = [event["bucket"], event["key"]]
+        columns = event["fields"]
+        s3 = boto3.client("s3")
+        byte_stream = io.BytesIO()
+
+        s3.download_fileobj(Bucket=os.environ["INGESTION_BUCKET"], Key=os.environ["INGESTION_KEY"], Fileobj=byte_stream)
+        byte_stream.seek(0)
+
+        result = obfuscate(columns, byte_stream)
+    
+        o_key = i_filepath[1].rsplit(".", 1)
+        o_key[0] += "-obfuscated."
+        o_key = "".join(o_key)
+
+        s3.put_object(Bucket=os.environ["OBFUSCATED_BUCKET"], Body=result, Key=o_key)
+
+        return { 
+            "response": 200,
+            "bucket": os.environ["OBFUSCATED_BUCKET"],
+            "key": o_key
+        }
+
+    except Exception as e:
+        return {"response": 500, "error": e}
+
---
+++ test/test_example_lambda.py
@@ -0,0 +1,58 @@
+from moto import mock_aws
+from src.example_lambda import lambda_handler
+
+import boto3
+import pytest
+import os
+
+
+@pytest.fixture(scope="function")
+def dummy_s3():
+    ingestion_bucket = "test-ingestion"
+    obfuscated_bucket = "test-obfuscated"
+    test_good_data = "id,First Name,Last Name,Age\n1,aaa,aaa,20\n2,bbb,bbb,302"
+    test_good_columns = ["id", "First Name", "Last Name", "Age"]
+    test_key = "dummy.csv"
+    
+    os.environ['INGESTION_BUCKET'] = ingestion_bucket
+    os.environ['OBFUSCATED_BUCKET'] = obfuscated_bucket
+    os.environ['INGESTION_KEY'] = test_key
+
+    with mock_aws():
+        s3 = boto3.client("s3")
+        s3.create_bucket(
+            Bucket=ingestion_bucket,
+            CreateBucketConfiguration={"LocationConstraint": "eu-west-2"},
+        )
+        s3.put_object(Bucket=ingestion_bucket, Body=test_good_data, Key=test_key)
+        s3.create_bucket(
+            Bucket=obfuscated_bucket,
+            CreateBucketConfiguration={"LocationConstraint": "eu-west-2"},
+        )
+        yield s3
+
+
+class TestFunctionLambdaHandler:
+    def test_function_creates_obfuscated_csv_in_target_bucket(self, dummy_s3):
+        test_event = {
+            "bucket": "test-ingestion",
+            "key": "dummy.csv",
+            "fields": ["id", "First Name", "Last Name", "Age"],
+        }
+        
+        response = lambda_handler(test_event, {})
+        check = dummy_s3.list_objects(Bucket="test-obfuscated") 
+        
+        assert response["response"] == 200
+        assert check["Contents"][0]["Key"] == response["key"]
+
+    def test_function_responds_500_if_error(self, dummy_s3):
+        test_event = {
+            "bucket": "test-ingestion",
+            "fields": ["id", "First Name", "Last Name", "Age"],
+        }
+
+        response = lambda_handler(test_event, {})
+
+        assert response["response"] == 500
+
